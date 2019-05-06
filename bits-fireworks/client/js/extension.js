3e10b2b28e79505cd00b27c00061b2c1e14cf4a2
Val Polouchkine <vpolouch@twitch.tv>
2019-05-06 12:22:31 -0700

    initial commit

---
+++ bits-fireworks/client/js/extension.js
@@ -0,0 +1,15 @@
+log = function () {
+    let prefix = "bits-fireworks: ";
+    return Function.prototype.bind.call(console.log, console, prefix);
+}();
+
+window.requestAnimFrame = (function () {
+    return window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.oRequestAnimationFrame ||
+        window.msRequestAnimationFrame ||
+        function (callback) {
+            window.setTimeout(callback, 1000 / 60);
+        };
+})();
